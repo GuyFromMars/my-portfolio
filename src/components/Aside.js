b7add74fe09e260e7ad1f3327fdcaa6587ad7114
GuyFromMars <popezoi192@gmail.com>
2024-09-28 22:47:26 +0000

    new 2

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,35 +1,14 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Link from "next/link";
-import { initFlowbite } from "flowbite";
 
-const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        closeDrawer();
-      }
-    }
-
-    if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen, ref]);
-
-  useEffect(() => {
-    initFlowbite();
-  }, []);
+const Aside = () => {
   return (
     <>
       <button
-        onClick={openDrawer}
         data-drawer-target="default-sidebar"
+        data-drawer-toggle="default-sidebar"
+        aria-controls="default-sidebar"
         type="button"
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
       >
@@ -49,10 +28,7 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
         </svg>
       </button>
       <aside
-        ref={ref}
-        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        } sm:translate-x-0 side-drawer`}
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform"
         id="default-sidebar"
         aria-label="Sidebar"
       >
@@ -60,7 +36,7 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
           <ul className="space-y-2 font-medium">
             <li>
               <Link
-                onClick={closeDrawer}
+                data-drawer-toggle="default-sidebar"
                 href="/"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
@@ -74,14 +50,13 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
-                <span onClick={closeDrawer} className="ms-3">
+                <span data-drawer-toggle="default-sidebar" className="ms-3">
                   About
                 </span>
               </Link>
             </li>
             <li>
               <Link
-                onClick={closeDrawer}
                 href="experience"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
@@ -94,17 +69,13 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
                 >
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                 </svg>
-                <span
-                  onClick={closeDrawer}
-                  className="flex-1 ms-3 whitespace-nowrap"
-                >
+                <span className="flex-1 ms-3 whitespace-nowrap">
                   Experience
                 </span>
               </Link>
             </li>
             <li>
               <Link
-                onClick={closeDrawer}
                 href="projects"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
@@ -117,17 +88,11 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
                 >
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
                 </svg>
-                <span
-                  onClick={closeDrawer}
-                  className="flex-1 ms-3 whitespace-nowrap"
-                >
-                  Projects
-                </span>
+                <span className="flex-1 ms-3 whitespace-nowrap">Projects</span>
               </Link>
             </li>
             <li>
               <Link
-                onClick={closeDrawer}
                 href="skills"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
@@ -140,17 +105,11 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
                 >
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                 </svg>
-                <span
-                  onClick={closeDrawer}
-                  className="flex-1 ms-3 whitespace-nowrap"
-                >
-                  Skills
-                </span>
+                <span className="flex-1 ms-3 whitespace-nowrap">Skills</span>
               </Link>
             </li>
             <li>
               <Link
-                onClick={closeDrawer}
                 href="interests"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
@@ -163,12 +122,7 @@ const Aside = ({ isOpen, closeDrawer, openDrawer }) => {
                 >
                   <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
                 </svg>
-                <span
-                  onClick={closeDrawer}
-                  className="flex-1 ms-3 whitespace-nowrap"
-                >
-                  Interests
-                </span>
+                <span className="flex-1 ms-3 whitespace-nowrap">Interests</span>
               </Link>
             </li>
           </ul>
