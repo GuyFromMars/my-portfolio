d7361bf111108e13b972fe450802efe128292033
GuyFromMars <popezoi192@gmail.com>
2024-09-29 10:50:58 +0000

    v 3.3.1

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -79,6 +79,7 @@ const Aside = () => {
             <li>
               <Link
                 href="/"
+                data-drawer-hide="logo-sidebar"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
